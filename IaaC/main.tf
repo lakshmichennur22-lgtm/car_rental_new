0ecda346cf240c5b5ed6caf1d1707e449393a699
lakshmi <lakshmichennur22@gmail.com>
2025-12-31 23:14:21 +0530

    new modified main.tf file

--- IaaC/main.tf
+++ IaaC/main.tf
@@ -2,10 +2,11 @@
 # LOCALS
 ########################################
 locals {
-  name_prefix = "${var.project}-${var.application}-${var.environment}-${var.location_short}"
-
-  # ALB-safe name (no underscores)
-  alb_name = substr(replace("${local.name_prefix}-alb", "_", "-"), 0, 32)
+  name_prefix = replace(
+    "${var.project}-${var.application}-${var.environment}-${var.location_short}",
+    "_",
+    "-"
+  )
 
   tags = {
     project     = var.project
@@ -58,16 +59,138 @@ resource "aws_subnet" "private2" {
 }
 
 ########################################
-# INTERNET + NAT
+# INTERNET GATEWAY
 ########################################
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags   = local.tags
 }
 
+########################################
+# ROUTE TABLES
+########################################
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+  tags   = local.tags
+}
+
+resource "aws_route" "public_default" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+  tags   = local.tags
+}
+
+resource "aws_route_table_association" "private1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+resource "aws_route_table_association" "private2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+########################################
+# NAT GATEWAY (FIXED)
+########################################
 resource "aws_eip" "nat" {
   domain = "vpc"
   tags   = local.tags
 }
 
-resource "aws_nat_gateway"
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public1.id
+  tags          = local.tags
+}
+
+resource "aws_route" "private_default" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+########################################
+# SECURITY GROUPS
+########################################
+resource "aws_security_group" "alb_sg" {
+  name   = "${local.name_prefix}-alb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+resource "aws_security_group" "frontend_sg" {
+  name   = "${local.name_prefix}-frontend-sg"
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+resource "aws_security_group" "backend_sg" {
+  name   = "${local.name_prefix}-backend-sg"
+  vpc_id = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.tags
+}
+
+########################################
+# ALB
+########################################
+resource "aws_lb" "alb" {
+  name               = substr("${local.name_prefix}-alb", 0, 20)
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+  tags               = local.tags
+}
+
+########################################
+# ECS CLUSTER
+########################################
+resource "aws_ecs_cluster" "cluster" {
+  name = "${local.name_prefix}-cluster"
+}
